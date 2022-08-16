c1279ceaa1047c2d2e548bfb208536307025ed06
jmartinmsft <72947063+jmartinmsft@users.noreply.github.com>
2022-08-16 11:47:21 -0400

    Bug fix
    No longer needs to copy files from the host to the guest.

--- Start-Setup.ps1
+++ Start-Setup.ps1
@@ -1,35 +1,47 @@
-﻿#
-# Start-Setup.ps1
-# Modified 2020/10/30
-# Last Modifier:  Jim Martin
-# Project Owner:  Jim Martin
-# Version: v1.0
-
-# Script should automatically start when the virtual machine starts.
-# Syntax for running this script:
-#
-# .\Start-Setup.ps1
-#
-#
-##############################################################################################
-#
-# This script is not officially supported by Microsoft, use it at your own risk.
-# Microsoft has no liability, obligations, warranty, or responsibility regarding
-# any result produced by use of this file.
-#
-##############################################################################################
-# The sample scripts are not supported under any Microsoft standard support
-# program or service. The sample scripts are provided AS IS without warranty
-# of any kind. Microsoft further disclaims all implied warranties including, without
-# limitation, any implied warranties of merchantability or of fitness for a particular
-# purpose. The entire risk arising out of the use or performance of the sample scripts
-# and documentation remains with you. In no event shall Microsoft, its authors, or
-# anyone else involved in the creation, production, or delivery of the scripts be liable
-# for any damages whatsoever (including, without limitation, damages for loss of business
-# profits, business interruption, loss of business information, or other pecuniary loss)
-# arising out of the use of or inability to use the sample scripts or documentation,
-# even if Microsoft has been advised of the possibility of such damages
-##############################################################################################
+﻿<#
+//***********************************************************************
+//
+// Start-Setup.ps1
+// Modified 16 August 2022
+// Last Modifier:  Jim Martin
+// Project Owner:  Jim Martin
+// Version: v20220816
+//
+//NOTE: Script should automatically start when the virtual machine starts.
+//Syntax for running this script:
+//
+// .\Start-Setup.ps1
+//
+//**********************************************************************​
+//***********************************************************************
+//
+// Copyright (c) 2018 Microsoft Corporation. All rights reserved.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//**********************************************************************​
+#>
+Clear-Host
+Write-Host -ForegroundColor Yellow '//***********************************************************************'
+Write-Host -ForegroundColor Yellow '//'
+Write-Host -ForegroundColor Yellow '// Copyright (c) 2018 Microsoft Corporation. All rights reserved.'
+Write-Host -ForegroundColor Yellow '//'
+Write-Host -ForegroundColor Yellow '// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR'
+Write-Host -ForegroundColor Yellow '// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,'
+Write-Host -ForegroundColor Yellow '// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE'
+Write-Host -ForegroundColor Yellow '// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER'
+Write-Host -ForegroundColor Yellow '// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,'
+Write-Host -ForegroundColor Yellow '// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN'
+Write-Host -ForegroundColor Yellow '// THE SOFTWARE.'
+Write-Host -ForegroundColor Yellow '//'
+Write-Host -ForegroundColor Yellow '//**********************************************************************​'
+Start-Sleep -Seconds 2
 
 Write-Host "Running the Start-Setup script..." -ForegroundColor Yellow
 Import-LocalizedData -BindingVariable UserCreds_LocalizedStrings -FileName "Sysprep-strings.psd1" -BaseDirectory C:\Temp
@@ -57,11 +69,11 @@ $UserName = $UserCreds_LocalizedStrings.res_0002
 [PSCredential]$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName,$securePwd
 
 ## Get VM Host information
-$physicalHostname = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters").PhysicalHostName
-$scriptFiles = "\\$physicalHostname\c$\Temp"
-New-PSDrive -Name "Script" -PSProvider FileSystem -Root $scriptFiles -Credential $credential
-Move-Item -Path "Script:\$ServerName*" -Destination C:\Temp
-Copy-Item -Path "Script:\DeployServer*.ps1" -Destination C:\Temp
+#$physicalHostname = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters").PhysicalHostName
+#$scriptFiles = "\\$physicalHostname\c$\Temp"
+#New-PSDrive -Name "Script" -PSProvider FileSystem -Root $scriptFiles -Credential $credential
+#Move-Item -Path "Script:\$ServerName*" -Destination C:\Temp
+#Copy-Item -Path "Script:\DeployServer*.ps1" -Destination C:\Temp
 
 Write-Host "Rebooting server..." -ForegroundColor Yellow
 Start-Sleep -Seconds 3
